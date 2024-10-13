7d02228c3f34f395721d640b53699b2530bcc271
Jacob Fielding <jf0517@gmail.com>
2024-10-13 14:43:50 -0700

    Downgraded swift tools for better compatibility

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 import PackageDescription
 
 let package = Package(
     name: "ValhallaModels",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(
             name: "ValhallaModels",
