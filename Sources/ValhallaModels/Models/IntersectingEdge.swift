cb7c8f3539d9e7520621cf56a61461735e6c4e50
Scott Shawcroft <scott@chickadee.tech>
2025-06-24 15:31:39 -0700

    Add names to intersecting edges

--- Sources/ValhallaModels/Models/IntersectingEdge.swift
+++ Sources/ValhallaModels/Models/IntersectingEdge.swift
@@ -17,16 +17,19 @@ public struct IntersectingEdge: Codable, Hashable {
     public var fromEdgeNameConsistency: Bool?
     /** True if this intersecting edge at the end node has consistent names with the path to the other edge. */
     public var toEdgeNameConsistency: Bool?
+    /** A list of street names for the intersecting edge */
+    public var names: [String]?
     public var driveability: Traversability?
     public var cyclability: Traversability?
     public var walkability: Traversability?
     public var use: EdgeUse?
     public var roadClass: RoadClass?
 
-    public init(beginHeading: Int? = nil, fromEdgeNameConsistency: Bool? = nil, toEdgeNameConsistency: Bool? = nil, driveability: Traversability? = nil, cyclability: Traversability? = nil, walkability: Traversability? = nil, use: EdgeUse? = nil, roadClass: RoadClass? = nil) {
+    public init(beginHeading: Int? = nil, fromEdgeNameConsistency: Bool? = nil, toEdgeNameConsistency: Bool? = nil, names: [String]? = nil, driveability: Traversability? = nil, cyclability: Traversability? = nil, walkability: Traversability? = nil, use: EdgeUse? = nil, roadClass: RoadClass? = nil) {
         self.beginHeading = beginHeading
         self.fromEdgeNameConsistency = fromEdgeNameConsistency
         self.toEdgeNameConsistency = toEdgeNameConsistency
+        self.names = names
         self.driveability = driveability
         self.cyclability = cyclability
         self.walkability = walkability
@@ -38,6 +41,7 @@ public struct IntersectingEdge: Codable, Hashable {
         case beginHeading = "begin_heading"
         case fromEdgeNameConsistency = "from_edge_name_consistency"
         case toEdgeNameConsistency = "to_edge_name_consistency"
+        case names
         case driveability
         case cyclability
         case walkability
@@ -52,6 +56,7 @@ public struct IntersectingEdge: Codable, Hashable {
         try container.encodeIfPresent(beginHeading, forKey: .beginHeading)
         try container.encodeIfPresent(fromEdgeNameConsistency, forKey: .fromEdgeNameConsistency)
         try container.encodeIfPresent(toEdgeNameConsistency, forKey: .toEdgeNameConsistency)
+        try container.encodeIfPresent(names, forKey: .names)
         try container.encodeIfPresent(driveability, forKey: .driveability)
         try container.encodeIfPresent(cyclability, forKey: .cyclability)
         try container.encodeIfPresent(walkability, forKey: .walkability)
